5d91fe1265e324c11d3080b41089fc9ad4e85feb
Im_Vikasdeep <vikasdeep@slt.work>
2025-04-09 21:05:59 +0530

    prod build bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -21,23 +22,23 @@ export default function Home() {
                 </div>
                 <div className='flex mt-15 px-5 text-s flex-wrap gap-4'>
                     <div className='flex-grow-1 bg-blue-200/10 border-blue-500/20 border px-5 py-2 rounded-4xl flex items-center text-blue-400 hover:bg-blue-500/20 hover:boder-blue-500/30 duration-300'>
-                        <img className='shrink-0' height={30} width={20} src="/assets/media/skills/icons8-angularjs-200.png" alt="angular" />
+                        <Image className='shrink-0' height={30} width={20} src="/assets/media/skills/icons8-angularjs-200.png" alt="angular" />
                         <span className='ml-3'>Angular</span>
                     </div>
                     <div className='flex-grow-1 bg-blue-200/10 border-blue-500/20 border px-5 py-2 rounded-4xl flex items-center text-blue-400 hover:bg-blue-500/20 hover:boder-blue-500/30 duration-300'>
-                        <img className='shrink-0' height={30} width={20} src="/assets/media/skills/favicon.ico" alt="Next.js" />
+                        <Image className='shrink-0' height={30} width={20} src="/assets/media/skills/favicon.ico" alt="Next.js" />
                         <span className='ml-3'>Next.js</span>
                     </div>
                     <div className='flex-grow-1 bg-blue-200/10 border-blue-500/20 border px-5 py-2 rounded-4xl flex items-center text-blue-400 hover:bg-blue-500/20 hover:boder-blue-500/30 duration-300'>
-                        <img className='shrink-0' height={30} width={20} src="/assets/media/skills/icons8-javascript-200.png" alt="JavaScript" />
+                        <Image className='shrink-0' height={30} width={20} src="/assets/media/skills/icons8-javascript-200.png" alt="JavaScript" />
                         <span className='ml-3'>JavaScript</span>
                     </div>
                     <div className='flex-grow-1 bg-blue-200/10 border-blue-500/20 border px-5 py-2 rounded-4xl flex items-center text-blue-400 hover:bg-blue-500/20 hover:boder-blue-500/30 duration-300'>
-                        <img className='shrink-0' height={30} width={20} src="/assets/media/skills/icons8-tailwind-css-200.png" alt="Tailwind CSS" />
+                        <Image className='shrink-0' height={30} width={20} src="/assets/media/skills/icons8-tailwind-css-200.png" alt="Tailwind CSS" />
                         <span className='ml-3'>Tailwind CSS</span>
                     </div>
                     <div className='flex-grow-1 bg-blue-200/10 border-blue-500/20 border px-5 py-2 rounded-4xl flex items-center text-blue-400 hover:bg-blue-500/20 hover:boder-blue-500/30 duration-300'>
-                        <img className='shrink-0' height={30} width={30} src="/assets/media/skills/icons8-sass-200.png" alt="SCSS" />
+                        <Image className='shrink-0' height={30} width={30} src="/assets/media/skills/icons8-sass-200.png" alt="SCSS" />
                         <span className='ml-3'>SCSS</span>
                     </div>
                 </div>
@@ -47,7 +48,7 @@ export default function Home() {
                     <h2 className='text-pink-600 text-3xl mb-5'>About me</h2>
                     <p className='mb-4'>Hi! I&apos;m a passionate Frontend Developer who loves crafting beautiful and user-friendly websites. I enjoy turning ideas into interactive and visually appealing experiences on the web.
                     </p>
-                    <p className='mb-4'>I&apos;m skilled in Angular, JavaScript, and SCSS, and I&apos;ve also explored Tailwind CSS and Next.js to expand my frontend toolkit. Whether it's building dynamic web apps or designing sleek UIs, I&apos;m always excited to bring creativity and clean code together.</p>
+                    <p className='mb-4'>I&apos;m skilled in Angular, JavaScript, and SCSS, and I&apos;ve also explored Tailwind CSS and Next.js to expand my frontend toolkit. Whether it&apos;s building dynamic web apps or designing sleek UIs, I&apos;m always excited to bring creativity and clean code together.</p>
                     <p className='mb-4'>Let&apos;s build something awesome!</p>
                 </div>
             </div>
@@ -59,17 +60,17 @@ export default function Home() {
                     <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
                         <div className="group border-blue-500/20 border p-8 pb-6 rounded-2xl bg-blue-900/10 hover:bg-blue-800/10 hover:boder-blue-500/30 duration-300">
                             <div className="flex items-start justify-between">
-                                {/* <img className='shrink-0' width={120} height={40} src="https://moviebase-angular.netlify.app/assets/images/logo.png" alt="logo" /> */}
+                                {/* <Image className='shrink-0' width={120} height={40} src="https://moviebase-angular.netlify.app/assets/images/logo.png" alt="logo" /> */}
                                 <div className='mr-4'>
                                     <h3>Moviebase</h3>
                                     <p className='text-s'>Movie Explorer App</p>
                                 </div>
                                 <div className='shrink-0 flex gap-2'>
                                     <a href="https://github.com/imvikasdeep/moviebase-angular" target='_blank'>
-                                        <img className='opacity-70 hover:opacity-100 duration-300' width={22} height={22} src="./assets/media/github-white.png" alt="github" />
+                                        <Image className='opacity-70 hover:opacity-100 duration-300' width={22} height={22} src="./assets/media/github-white.png" alt="github" />
                                     </a>
                                     <a href="https://moviebase-angular.netlify.app/" target='_blank'>
-                                        <img className='opacity-70 hover:opacity-100 duration-300' width={22} height={22} src="./assets/media/globe-white.png" alt="globe" />
+                                        <Image className='opacity-70 hover:opacity-100 duration-300' width={22} height={22} src="./assets/media/globe-white.png" alt="globe" />
                                     </a>
                                 </div>
                             </div>
@@ -91,22 +92,22 @@ export default function Home() {
                     <ul className='flex gap-5 justify-center'>
                         <li>
                             <a href="https://github.com/imvikasdeep" target='_blank'>
-                                <img width={25} height={25} src="./assets/media/github-white.png" alt="github" />
+                                <Image width={25} height={25} src="./assets/media/github-white.png" alt="github" />
                             </a>
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/in/imvikasdeep/" target='_blank'>
-                                <img width={25} height={25} src="./assets/media/linkedin-white.png" alt="github" />
+                                <Image width={25} height={25} src="./assets/media/linkedin-white.png" alt="github" />
                             </a>
                         </li>
                         <li>
                             <a href="https://x.com/Im_vikasdeep" target='_blank'>
-                                <img width={25} height={25} src="./assets/media/x-com-white.png" alt="github" />
+                                <Image width={25} height={25} src="./assets/media/x-com-white.png" alt="github" />
                             </a>
                         </li>
                         <li>
                             <a href="https://codepen.io/im_vikasdeep" target='_blank'>
-                                <img width={25} height={25} src="./assets/media/codepen-white.png" alt="github" />
+                                <Image width={25} height={25} src="./assets/media/codepen-white.png" alt="github" />
                             </a>
                         </li>
                     </ul>
