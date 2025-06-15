e8dcaaf607373f73410b77566310d5acb9a2bfa0
Im_Vikasdeep <vikasdeep@slt.work>
2025-06-15 22:23:45 +0530

    UI update

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -6,7 +6,7 @@ function Header() {
         <header className='z-10 p-4 shadow w-full'>
             <div className="xl:max-w-8/10 flex justify-between items-center mx-auto">
                 <Link href='/' className='mb-0 text-white font-semi-bold text-xl tracking-wide'>VIKASDEEP CHAUDHARY</Link>
-                <a className='shadow-[0_0_20px_0] shadow-cyan-500/50 inline-block bg-blue-800/40 border-blue-400/20 border hover:bg-blue-400/10 text-white duration-300 py-2 px-5 rounded-xl' href="mailto:vikasdeep3098@gmail.com">Get in touch</a>
+                <a className='inline-block border-violet-700 border hover:bg-blue-500/20 text-violet-700 duration-300 py-2 px-5 rounded-xl' href="mailto:vikasdeep3098@gmail.com">Get in touch</a>
             </div>
         </header>
     )
